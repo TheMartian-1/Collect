c5bd2dcb2e0009bb197df2b90f25dcef3a1b901c
{Marshall} <{themarshallmash@gmail.com}>
2020-11-04 16:52:39 -0600

    Added answers for all questions but #1

--- Collect/src/main.java
+++ Collect/src/main.java
@@ -1,8 +1,33 @@
+import java.util.HashMap;
+import java.util.TreeMap;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("start");
+		
+		HashMap<String, String> userPass = new HashMap<String,String>();
+		userPass.put("joeBiden270", "democrat4Life");
+		userPass.put("THEdonald", "maga2020");
+		
+		System.out.println(userPass);
+		
+		TreeMap<Integer, String> states = new TreeMap<Integer, String>();
+		states.put(1, "Delaware");
+		states.put(28, "Texas");
+		states.put(3, "Old Jersey");
+		states.put(3, "New Jersey");
+		
+		System.out.println(states);
+		
+		/*
+		 * The code above demonstrates the similarities between HashMap and TreeMap
+		 * When simply adding and printing the methods, they appear to act much in the same way.
+		 * But, the TreeMap will actually automatically sort the TreeMap by the provided args.
+		 * Also shown in the TreeMap is that a single Integer can't have two different Strings attached to it. 
+		 * This works in the same way for HashMap.
+		 * If you want to guarantee the order of key then TreeMap should be used as HashMap has no built-in guarantee. 
+		 * 
+		 */
 
 	}
 
